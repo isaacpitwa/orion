8a8ae4237867954f939414bd650dc78aa6fae521
Isaac Pitwa <isaacpitwa256@gmail.com>
2023-10-14 10:54:32 +0300

    Initial commit

---
+++ src/navigation/BottomNavigator.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import {screens as bottomScreens} from '../screens';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+
+const BottomTab = createBottomTabNavigator();
+
+export default function MyTabs() {
+  return (
+    <BottomTab.Navigator
+      initialRouteName="Post"
+      screenOptions={{
+        tabBarActiveTintColor: '#e91e63',
+      }}>
+      <BottomTab.Screen name="Feed" component={bottomScreens.Feeds} />
+      <BottomTab.Screen name="Post" component={bottomScreens.AddPost} />
+      <BottomTab.Screen name="Account" component={bottomScreens.Account} />
+    </BottomTab.Navigator>
+  );
+}
---
+++ src/screens/AddPost.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const AddPostScreen = () => {
+  return (
+    <View>
+      <Text>Add Post Screen</Text>
+    </View>
+  );
+};
+
+export default AddPostScreen;
